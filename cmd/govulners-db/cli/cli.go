a41b8b8529d1be09d30df25d94047ba44ed2cf52
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-05-16 11:04:02 -0700

    Create cli.go
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

---
+++ cmd/govulners-db/cli/cli.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/nextlinux/govulners-db/cmd/govulners-db/application"
+	"github.com/nextlinux/govulners-db/cmd/govulners-db/cli/commands"
+)
+
+type config struct {
+	app *application.Application
+}
+
+type Option func(*config)
+
+func WithApplication(app *application.Application) Option {
+	return func(config *config) {
+		config.app = app
+	}
+}
+
+func New(opts ...Option) *cobra.Command {
+	cfg := &config{
+		app: application.New(),
+	}
+	for _, fn := range opts {
+		fn(cfg)
+	}
+
+	app := cfg.app
+
+	cache := commands.Cache(app)
+	cache.AddCommand(commands.CacheStatus(app))
+	cache.AddCommand(commands.CacheDelete(app))
+	cache.AddCommand(commands.CacheBackup(app))
+	cache.AddCommand(commands.CacheRestore(app))
+
+	root := commands.Root(app)
+	root.AddCommand(commands.Version(app))
+	root.AddCommand(commands.Pull(app))
+	root.AddCommand(commands.Build(app))
+	root.AddCommand(commands.Package(app))
+	root.AddCommand(commands.ListProviders(app))
+	root.AddCommand(cache)
+
+	return root
+}
